8532cbb42eb7a129a4e62182943a096e7c254b66
Panzzz23 <maria.jimenez544@soy.sena.edu.co>
2024-09-28 14:40:08 -0500

    Update App.js

--- ejercicio2/src/App.js
+++ ejercicio2/src/App.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import FormularioUsuario from './FormularioUsuario';
 
-function App() {
-  return (
-    <div className="App">
-      <FormularioUsuario />
-    </div>
-  );
-}
-
-export default App;
