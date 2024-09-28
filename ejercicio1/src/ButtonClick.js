60c52ddf4e0f8b859fa29e013194f97b9622b992
Panzzz23 <maria.jimenez544@soy.sena.edu.co>
2024-09-28 14:09:14 -0500

    ejercicio 1 Maria Jose Jimenez Diaz
    aqui esta el ejercicio 1 donde se hizo un alert con un modal para que apareciera despues de clickear un boton

--- ejercicio1/src/ButtonClick.js
+++ ejercicio1/src/ButtonClick.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import { Modal, Button } from 'react-bootstrap';
 
-const ButtonClick = () => {
-    const handleClick = () => { 
-        alert('Boton clikeado')
-    };
+    const ButtonClick = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShow = () => setShowModal(true);
+    const handleClose = () => setShowModal(false);
 
     return (
-        <div className="d-flex justify-content-center mt-5">
-            <button onClick={handleClick} className="btn btn-primary">Haz click en mi!!
-            </button>
+        <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+        <h1 className="text-center mb-4">Ejercicio 1: Manejo de eventos</h1>
+
+        <button className="btn btn-primary mb-3" onClick={handleShow}>
+            Haz clic aquí
+        </button>
+
+        <Modal show={showModal} onHide={handleClose} centered>
+            <Modal.Header closeButton>
+            <Modal.Title>¡Botón clickeado!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            holiii has clickeado el botón ahora vez este mensaje uwu 
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Cerrar
+            </Button>
+            </Modal.Footer>
+        </Modal>
         </div>
     );
-};
+    };
 
-export default ButtonClick;
+    export default ButtonClick;
