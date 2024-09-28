bc627e278458ae6b40f29e124346f38fb9efa1cd
Panzzz23 <maria.jimenez544@soy.sena.edu.co>
2024-09-28 14:40:43 -0500

    ejercicio 2 Maria Jose Jimenez Diaz

--- ejercicio2/src/App.js
+++ ejercicio2/src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import FormularioUsuario from './FormularioUsuario';
 
+function App() {
+  return (
+    <div className="App">
+      <FormularioUsuario />
+    </div>
+  );
+}
+
+export default App;
